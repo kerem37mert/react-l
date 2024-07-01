7182fbf0567fd150e1f7bb79556d90e1bb17a382
Kerem Mert <kerem37mert@yahoo.com>
2024-07-01 18:04:25 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return(
+        <div>
+            kerem
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div>
+            { 5 }
+        </div>
+    );
+}
+
+export default Home;
